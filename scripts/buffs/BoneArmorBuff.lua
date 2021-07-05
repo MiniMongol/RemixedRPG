34ffd3aea29b87b13473ab9f545dee9f0778d78a
МИНИ МОНГОЛ <insafnazipov2005@gmail.com>
2021-07-05 04:06:03 +0300

    First co

---
+++ scripts/buffs/BoneArmorBuff.lua
@@ -0,0 +1,83 @@
+---
+--- Created by Mongol
+--- VK: mongolinsult
+---
+
+local RPD  = require "scripts/lib/commonClasses"
+local RPG  = require "scripts/lib/Functions"
+local storage = require "scripts/lib/storage"
+
+local a = "bonearmor"
+local hero = RPD.Dungeon.hero
+local level
+local scale = 0
+local acc = 0
+
+local buff = require "scripts/lib/buff"
+
+return buff.init{
+    desc  = function ()
+        return {
+            icon          = 63,
+            name          = "Костяное облачение",
+            info          = "",
+        }
+    end,
+    charAct = function(self,buff)
+      scale = scale +1
+    end,
+    
+    defenceProc = function(self,buff,enemy,damage)
+      acc = acc - math.ceil(damage*0.5)
+      return damage
+    end,
+    
+    drBonus = function(self,buff)
+     local Count = storage.gameGet(a) or {}
+     return math.ceil(RPG.magStr()*(0.35 + 0.1*scale) + 2*Count.lvl)
+    end,
+    
+    speedMultiplier = function(self, buff)
+        return 0.7 + RPG.AllFast()*0.2
+    end,
+    
+    detach = function(self,buff)
+      hero = RPD.Dungeon.hero
+      level = RPD.Dungeon.level
+      if buff.target == hero then
+        if (hero:getSkillPoints() + acc) > hero:getSkillPointsMax() then
+          hero:setSkillPoints(hero:getSkillPointsMax())
+          acc = acc - (hero:getSkillPointsMax() - hero:getSkillPoints())
+        else
+          hero:setSkillPoints(hero:getSkillPoints() + acc)
+          acc = 0
+        end
+      else
+        if buff.target:hp() + acc > buff.target:ht() then
+          buff.target:hp(buff.target:ht())
+          acc = acc - (buff.target:ht() - buff.target:hp())
+        else
+          buff.target:heal(acc,buff.target)
+          acc = 0
+        end
+      end
+      local x = level:cellX(buff.target:getPos())
+      local y = level:cellY(buff.target:getPos())
+      local enName
+     
+      for i = x-1,x+1 do
+        for j = y-1,y+1 do
+          local pos = level:cell(i,j)
+          local enemy = RPD.Actor:findChar(pos)
+          if enemy and enemy ~= hero then
+            enName = enemy:getMobClassName()
+          else
+            enName = nil
+          end
+          if enemy and enemy ~= hero and (enName ~= "BoneGolem_lvl1" and enName ~= "BoneGolem_lvl2" and enName ~= "BoneGolem_lvl3") then
+            enemy:damage(acc, hero)
+          end
+        end
+      end
+    end
+}
---
+++ scripts/spells/PandemicLance.lua
@@ -0,0 +1,91 @@
+---
+--- Created by Mongol
+--- VK: mongolinsult
+---
+
+local RPD = require "scripts/lib/commonClasses"
+
+local RPG = require "scripts/lib/Functions"
+
+local spell = require "scripts/lib/spell"
+
+local storage = require "scripts/lib/storage"
+
+local lvl
+local expMax
+local exp
+local a = "pandemiclance"
+local pos
+local moveTo
+local enemy
+
+return spell.init{
+    desc  = function ()
+    local Count = storage.gameGet(a) or {}
+
+     if Count.lvl == nil then
+  lvl = 1
+  else
+   if lvl == nil then
+    lvl = Count.lvl
+   end
+  end
+        return {
+            image         = 35,
+            imageFile     = "spellsIcons/spellicons.png",
+            name          = "PandemicLanceN["..tostring(lvl).." уровень]",
+            info          = "PandemicLanceD",
+            magicAffinity = "Combat",
+            targetingType = "cell",
+            level         = 1,
+            spellCost     = 10,
+            cooldown      = math.max(16-lvl-RPG.spellFast,10-math.ceil(RPG.spellFast/2)),
+            castTime      = 1
+        }
+    end,
+    
+    castOnCell = function(self, spell, chr, cell, caster)
+    local level = RPD.Dungeon.level
+    local hero = RPD.Dungeon.hero
+     if RPD.Dungeon.hero:lvl() <= 4 then
+     RPD.glog("-- Для использования навыка вам нужен 5+ уровень")
+     return false
+    end
+      
+    Count = storage.gameGet(a) or {}
+    Spell = storage.gameGet("bloodspikes") or {}
+    if Count.lvl ~= nil then
+     lvl = Count.lvl
+     exp = Count.exp
+     expMax = Count.expMax
+     storage.gamePut(a,{exp = exp, expMax = expMax, lvl = lvl})
+    else
+     lvl = 1
+     exp = 0
+     expMax = 4
+     storage.gamePut(a,{exp = exp, expMax = expMax, lvl = lvl})
+    end
+    
+    if exp == expMax then
+       exp = 0
+       expMax = expMax+5
+       lvl = lvl+1
+     else
+       exp = exp+1
+     end
+     
+    local heroPos = hero:getPos()
+    pos = RPD.Ballistica:cast(heroPos,cell,true,false,false)
+    RPD.zapEffect(heroPos, pos, "horattack_effect")
+    for i = 0, math.min( math.min(2+(Spell.staks or 0), 6), RPD.Ballistica.distance ) do
+      enemy = RPD.Actor:findChar(RPD.Ballistica.trace[i])
+      if enemy and enemy ~= hero then
+        enemy:damage( math.ceil(RPG.magStr()*(0.25+0.05*Spell.staks)), hero)
+        buff = RPD.affectBuff(enemy, "PoisonBuff", Spell.staks or 0)
+        buff:level(RPG.magStr()*(0.8+0.15*Spell.staks))
+        RPD.topEffect(enemy:getPos(),"bleeding_effect")
+        end
+      end
+     return true
+   end
+}
