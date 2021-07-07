f74790ca608154082b5abf3b44077767db5781f0
МИНИ МОНГОЛ <insafnazipov2005@gmail.com>
2021-07-07 03:29:53 +0300

    Translating to english

--- scripts/items/Halberd.lua
+++ scripts/items/Halberd.lua
@@ -46,11 +46,11 @@ return item.init{
     info = function(self,item)
       hero = RPD.Dungeon.hero
       str = stra-item:level()
-      local info = RPD.textById("Halberd_Info").."\n\n"..RPD.textById("Halberd_Name")..RPD.textById("WeaponInfo0")..tier..RPD.textById("WeaponInfo1")..math.ceil((maxDmg+minDmg+tier*item:level()*2)/2)..RPD.textById("WeaponInfo2")..stra..RPD.textById("WeaponInfo3")..RPD.textById("WeaponAccuNotFast").."\n"..self.data.sInfo
+      local info = RPD.textById("Halberd_Info").."\n\n"..RPD.textById("Halberd_Name")..RPD.textById("WeaponInfo0")..tier..RPD.textById("WeaponInfo1")..math.ceil((maxDmg+minDmg+tier*item:level()*2)/2)..RPD.textById("WeaponInfo2")..stra..RPD.textById("WeaponInfo3")..RPD.textById("WeaponAccuNotFast").."\n\n"..self.data.sInfo
       if hero:STR() >= str then
         return info
       else
-        return info.."\n"..RPD.textById("WeaponLimit")
+        return info..RPD.textById("WeaponLimit")
       end
     end,
     
