2f2468ebd5c382b37a41fb10a556ff3db2f18b5b
МИНИ МОНГОЛ <insafnazipov2005@gmail.com>
2021-07-23 14:59:29 +0300

    Few fixes

--- scripts/items/Tonfa.lua
+++ scripts/items/Tonfa.lua
@@ -113,12 +113,12 @@ return item.init{
     
     accuracyFactor = function(self,item,user)
       str = math.max(stra-2*item:level(),1)
-      return 1.1 + RPG.itemStrBonus(str)
+      return 1 + RPG.itemStrBonus(str)
     end,
     
     attackDelayFactor = function(self,item,user)
       str = math.max(stra-2*item:level(),1)
-      return 0.9 - RPG.itemStrBonus(str)
+      return 1 - RPG.itemStrBonus(str)
     end,
     
     typicalSTR = function(self,item,user)
