9a76b30b07e9ee1999f7ea91fe024c61c7f814c4
Mikhael  Danilov <mikhael.danilov@gmail.com>
2023-04-06 01:47:06 +0300

    Cleanup

--- scripts/actors/TomOfMastery.lua
+++ scripts/actors/TomOfMastery.lua
@@ -8,40 +8,29 @@ local RPD = require "scripts/lib/commonClasses"
 
 local actor = require "scripts/lib/actor"
 
-local dungeonEntrance = {13, 4}
-local shopEntrance    = {18, 16}
+local dungeonEntrance = { 13, 4 }
+local shopEntrance = { 18, 16 }
 
 local lastWarningTime = 0
 local time = 0
 
 return actor.init({
-
-
     act = function()
-        
-      local levelW = RPD.Dungeon.level:getWidth()
-      local levelH = RPD.Dungeon.level:getHeight()
-      
-      for i = 0, levelW do
-        for j = 0, levelH do
-          local cell = RPD.Dungeon.level:cell(i,j)
-          local enemy = RPD.Actor:findChar(cell)
-        end 
-      end
-        
+        local hero = RPD.Dungeon.hero
+
         if hero:getBelongings():getItem("TomeOfMastery") ~= nil then
-         hero:getBelongings():getItem("TomeOfMastery"):detach(hero:getBelongings().backpack)
-    end
+            hero:getBelongings():getItem("TomeOfMastery"):detach(hero:getBelongings().backpack)
+        end
 
         return true
     end,
-    
-    
+
+
     actionTime = function()
         return 1
     end,
-    
-    
+
+
     activate = function()
     end
 })
